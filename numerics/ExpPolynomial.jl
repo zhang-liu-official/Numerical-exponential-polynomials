26d45e4b9add8c2822e826cc7bcab91aaa7bed03
Wang Yanhua <yanhua@u.yale-nus.edu.sg>
2020-06-12 16:24:34 +0800

    operators

--- numerics/ExpPolynomial.jl
+++ numerics/ExpPolynomial.jl
@@ -1,147 +1,54 @@
+include("ExpSum.jl")
 using SymPy
-
-struct ExpSum
-    ExpList::Array{ComplexF64}
-    CoeffList::Array{ComplexF64}
-end
-
-function ExpPolyConvert(ExpList::Array{ComplexF64}, CoeffList::Array{ComplexF64})
-    if (length(ExpList) != length(CoeffList))
-        error("Invalid Exponential Polynomial")
-    end
-
-    CheckList = Dict{ComplexF64,Int64}()
-    # exponent + index (at the first time it appears)
-
-    for i = 1:length(ExpList)
-        if haskey(CheckList, ExpList[i])
-            # if the same exponent already exists, add coefficient to the term where it first appears
-            CoeffList[CheckList[ExpList[i]]] = CoeffList[CheckList[ExpList[i]]] + CoeffList[i]
-            CoeffList[i] = 0
-        else
-            # create a new entry in the dictionary
-            CheckList[ExpList[i]] = i
-        end
-    end
-            
-    newExpList = ComplexF64[]
-    # empty list to store new ExpList values
-    newCoeffList = ComplexF64[]
-    # empty list to store new CoeffList values 
-            
-    for i = 1:length(CoeffList)
-        # make sure CoeffList has no zero
-        if (CoeffList[i] != 0)
-            append!(newCoeffList, CoeffList[i])
-            append!(newExpList, ExpList[i])
-        end
-    end
-    return (ExpSum(newExpList, newCoeffList))
-end
-
-# Operators 
-
-function Base.:+(x::ExpSum, y::ExpSum)
-    
-    # deepcopy avoids changing the ExpList of x     
-    newExpList = deepcopy(x.ExpList)
-    newCoeffList = deepcopy(x.CoeffList)
-    
-    for i = 1:length(y.ExpList)
-        append!(newExpList, y.ExpList[i])
-        append!(newCoeffList, y.CoeffList[i])
-    end
-    
-    ExpPolyConvert(newExpList, newCoeffList)
-        
-end
-
-function Base.:-(x::ExpSum, y::ExpSum)
-    
-    newExpList = deepcopy(x.ExpList)
-    newCoeffList = deepcopy(x.CoeffList) 
-    
-    for i = 1:length(y.ExpList)
-        append!(newExpList, y.ExpList[i])
-        append!(newCoeffList, -1.0 * y.CoeffList[i])
-    end
-    
-    ExpPolyConvert(newExpList, newCoeffList)
-           
-end
-
-function Base.:*(x::ExpSum, y::Number)
-    
-    y = convert(ComplexF64, y)
-    newCoeffList = ComplexF64[]
-    
-    for i = 1:length(x.CoeffList)
-        append!(newCoeffList, x.CoeffList[i] * y)
-    end
-    
-    return (ExpSum(x.ExpList, newCoeffList))
-        
-end
-
-function Base.:*(y::Number, x::ExpSum)
-    
-    y = convert(ComplexF64, y)
-    return (x * y)
-        
-end
-
-function Base.:*(x::ExpSum, y::ExpSum)
-    
-    newExpList = ComplexF64[]
-    # empty list to store new ExpList values
-    newCoeffList = ComplexF64[]
-    # empty list to store new CoeffList values 
-     
-    for i = 1:length(x.ExpList)
-        for j = 1:length(y.ExpList)
-            append!(newExpList, x.ExpList[i] + y.ExpList[j])
-            append!(newCoeffList, x.CoeffList[i] * y.CoeffList[j])
-        end
-    end
-    
-    ExpPolyConvert(newExpList, newCoeffList)
-    
-end
-
-# Evaluate exponential sum 
-
-function expSum(z::Number, x::ExpSum)
-    
-    z = convert(ComplexF64, z)
-    
-    expList = deepcopy(x.ExpList)
-    for i = 1:length(expList)
-        expList[i] *= z
-    end
-    
-    ExpPolyConvert(expList, x.CoeffList)
-    
-end
-
-(f::ExpSum)(z::Number) = expSum(z, f)
-
-# SymPy version 
-
-function expSumSym(z::Sym, x::ExpSum)
-                
-    symList = 0
-    for i = 1:length(x.ExpList)
-        symList += x.CoeffList[i] * exp(x.ExpList[i] * z)
-    end
-    
-    return symList
-    
-end
 
 struct ExpPolynomial
     ExpList::Array{ComplexF64}
-    PolyList::Array{Tuple}
+    PolyList::Array{Polynomial} 
 end
+
+function ExpPolyConvert(x::ExpPolynomial)
+    
+    ExpList = x.ExpList
+    PolyList = x.PolyList
+    
+    if(length(ExpList) != length(PolyList))
+        error("Invalid Exponential Polynomial")
+    end
+
+    CheckList = Dict{ComplexF64, Int64}()
+    
+    for i = 1:length(ExpList)
+        if haskey(CheckList, ExpList[i])
+            #if the same exponent already exists, add polynomial to the term where it first appears
+            
+            PolyList[CheckList[ExpList[i]]] += PolyList[i]
+            PolyList[i] = 0
+            
+            #the next if-statement will later ignore this term
+        else
+            #create a new entry in the dictionary
+            CheckList[ExpList[i]] = i
+        end
+    end
+                
+    newExpList = ComplexF64[]
+    #empty list to store new ExpList values
+    newPolyList = Polynomial[]
+    #empty list to store new PolyList values 
+        
+    for i = 1:length(PolyList)
+        #make sure CoeffList has no zero
+        if(PolyList[i] != 0)
+            push!(newPolyList, PolyList[i])
+            append!(newExpList, ExpList[i])
+        end
+    end
+        
+    return (ExpPolynomial(newExpList, newPolyList))   
+        
+end
+
+# Evaluate exponential polynomial
 
 function expPoly(z::Number, x::ExpPolynomial)
         
@@ -154,8 +61,8 @@ function expPoly(z::Number, x::ExpPolynomial)
     
     coeffList = ComplexF64[]
     for j = 1:length(x.PolyList)
-        temp = evalpoly(z, x.PolyList[j])
-        append!(coeffList, temp)
+        temp = x.PolyList[j]
+        append!(coeffList, temp(z))
     end
     
     ExpPolyConvert(expList, coeffList)
@@ -164,13 +71,15 @@ end
 
 (f::ExpPolynomial)(z::Number) = expPoly(z, f)
 
+# SymPy version
+
 function expPolySym(z::Sym, x::ExpPolynomial)
     
     coeffList = []
     for j = 1:length(x.PolyList)
         pol = 0
         for k = 1:length(x.PolyList[j])
-            pol += x.PolyList[j][k] * z^(k - 1)
+            pol += x.PolyList[j][k-1] * z^(k-1)
         end
         append!(coeffList, pol)
     end
---
+++ numerics/ExpSum.jl
@@ -0,0 +1,70 @@
+using SymPy
+
+struct ExpSum
+    ExpList::Array{ComplexF64}
+    CoeffList::Array{ComplexF64}
+end
+
+function ExpPolyConvert(ExpList::Array{ComplexF64}, CoeffList::Array{ComplexF64})
+    if (length(ExpList) != length(CoeffList))
+        error("Invalid Exponential Sum")
+    end
+
+    CheckList = Dict{ComplexF64,Int64}()
+    # exponent + index (at the first time it appears)
+
+    for i = 1:length(ExpList)
+        if haskey(CheckList, ExpList[i])
+            # if the same exponent already exists, add coefficient to the term where it first appears
+            CoeffList[CheckList[ExpList[i]]] = CoeffList[CheckList[ExpList[i]]] + CoeffList[i]
+            CoeffList[i] = 0
+        else
+            # create a new entry in the dictionary
+            CheckList[ExpList[i]] = i
+        end
+    end
+            
+    newExpList = ComplexF64[]
+    # empty list to store new ExpList values
+    newCoeffList = ComplexF64[]
+    # empty list to store new CoeffList values 
+            
+    for i = 1:length(CoeffList)
+        # make sure CoeffList has no zero
+        if (CoeffList[i] != 0)
+            append!(newCoeffList, CoeffList[i])
+            append!(newExpList, ExpList[i])
+        end
+    end
+    return (ExpSum(newExpList, newCoeffList))
+end
+
+# Evaluate exponential sum 
+
+function expSum(z::Number, x::ExpSum)
+    
+    z = convert(ComplexF64, z)
+    
+    expList = deepcopy(x.ExpList)
+    for i = 1:length(expList)
+        expList[i] *= z
+    end
+    
+    ExpPolyConvert(expList, x.CoeffList)
+    
+end
+
+(f::ExpSum)(z::Number) = expSum(z, f)
+
+# SymPy version 
+
+function expSumSym(z::Sym, x::ExpSum)
+                
+    symList = 0
+    for i = 1:length(x.ExpList)
+        symList += x.CoeffList[i] * exp(x.ExpList[i] * z)
+    end
+    
+    return symList
+    
+end
